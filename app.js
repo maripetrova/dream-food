7aa789127379d2e8a8b079310c318948c00e0f02
maripetrova <masha.petrova.27012004@gmail.com>
2021-11-17 16:46:55 +0300

    Create little server

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("<h1>Gream food!</h1>")
+    res.send("<h1>Dream food!</h1>")
 })
 
 app.listen(PORT);
