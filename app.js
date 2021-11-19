a8bdf55ec2be9c435e29ea789c7f2a4a1d1172c7
maripetrova <masha.petrova.27012004@gmail.com>
2021-11-19 13:10:15 +0300

    Правка от 19.11.2021

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const PORT = process.env.PORT || 3000;
-
+const serverRouter = require("./server/routes/mainRouter")
 const app = express();
+const stylus = require("stylus")
 
-app.get("/", (req, res) => {
-    res.send("<h1>Dream food!</h1>")
-})
+
+app.use(stylus.middleware({
+    src: "./public",
+    dest: "./public"
+}));
+
+app.set("views", "./server/views")
+app.set("view engine", "pug")
+
+app.use(express.static("./public"))
+
+app.use("/", serverRouter)
 
 app.listen(PORT);
